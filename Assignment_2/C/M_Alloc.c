c1fa0224cd2ff161307414956f549708c9dc274a
Spencer <spt631@usask.ca>
2021-10-15 21:31:55 -0600

    Now properly detecting which memory blocks (next or previous) can be coalesced. Significant commenting and code clean up.

--- Assignment_2/C/M_Alloc.c
+++ Assignment_2/C/M_Alloc.c
@@ -1,19 +1,12 @@
-/*
- void *M_Alloc(int size):
-	M_Alloc() is used to allocate a chunk;
-		its argument is the requested size in bytes.
-
-	The function should return a pointer to the start of the allocated chunk, or NULL if the request fails,
-			e.g. satisfying the request is not possible owing to there not being enough contiguous free space.
-
-	The actual size of the allocated chunk should be size rounded up to the nearest multiple of 16 bytes.
-
-	Allocation should use the next fit policy.
- */
-
 #include "M_Alloc.h"
 
-
+/* PURPOSE: To allocate
+ * PRE-CONDITIONS:
+ *              int size --> Size (in bytes) you wish to allocate for your data.
+ *              Must have called M_Init before using.
+ * POST-CONDITIONS: The block of memory at the pointer is cleared and declared writable.
+ * RETURN: A void pointer to the writable portion of the newly allocated block.
+ */
 void *M_Alloc(int size)
 {
     long unsigned memChunks = size/16;
@@ -23,72 +16,27 @@ void *M_Alloc(int size)
     }
     memChunks = memChunks * 16;
 
-
-
-    //Check for magic number:
-//    if (currentBlock->memptr == magicNumber)
-//    {
-//        printf("Current block [%p with size %lu] "
-//               "points to magic number\n",currentBlock,currentBlock->size);
-//    }
-//    else
-//    {
-//        printf("Current block [%p with size %lu] DOES NOT point to magic number! "
-//               "(points to: [%p with size %lu])!\n",currentBlock,currentBlock->size,currentBlock->memptr,currentBlock->memptr->size);
-//        if (currentBlock->memptr->size == 0)
-//        {
-//            printf("END OF LIST REACHED");
-//            return NULL;
-//        }
-//    }
-
-
     printf("Current address: [%p] size: [%lu]  points to:[%p]\n",currentBlock,currentBlock->size,currentBlock->memptr);
 
 
-    //Create a new block.
-
-    //Header:
+    //Detect header and footer.
     memStruct *header = (void*) currentBlock;
-
-    //Footer:
     memStruct *footer = (void*) currentBlock + 16 + memChunks; //Move past the header + allocated length of node.
 
     //Set footer values:
     footer->size = currentBlock->size - (memChunks + 16); //Subtract size of node + room for footer from the free space.
     footer->memptr = currentBlock;
-
-    //Set header values.
+      //Set header values.
     header->size = memChunks;
-    //header->memptr = currentBlock + memChunks;
     header->memptr = footer;
 
 
-    //footer->size = currentBlock->size - memChunks;
+    printf("\tNew header address: [%p] size: [%lu]  points to:[%p]\n",header,header->size,header->memptr);
+    printf("\tNew footer address: [%p] size: [%lu]  points to:[%p]\n\n",footer,footer->size,footer->memptr);
 
-
-
-    printf("Header address: [%p] size: [%lu]  points to:[%p]\n",header,header->size,header->memptr);
-    printf("Footer address: [%p] size: [%lu]  points to:[%p]\n\n",footer,footer->size,footer->memptr);
-
-
-//    currentBlock->size = memChunks; //How big is the new block?
-//    currentBlock->memptr = currentBlock + memChunks; //Where is next header?
-//
-    //currentBlock->memptr->memptr = magicNumber; //Next's header should be a magic number.
-//    currentBlock->memptr->size = memChunks;
 
     void* out = currentBlock;
     currentBlock = currentBlock->memptr;
     currentBlock->memptr = magicNumber;
     return out+16;
-    //return out+16;
 }
-//
-//int main(int argc, char *argv[])
-//{
-//
-//    M_Init(1590);
-//    M_Alloc(30);
-//    return 0;
-//}
--- Assignment_2/C/m_init.c
+++ Assignment_2/C/m_init.c
@@ -8,12 +8,18 @@
 	The function should return 0 on success and -1 for a failure return.
 
 */
+
 #include "M_Init.h"
 
+/* PURPOSE:
+ * PRE-CONDITIONS:
+ * POST-CONDITIONS:
+ * RETURN:
+ */
 int M_Init(int size)
 {
-    printf("Function called\n");
-    if (freeList != NULL) //freeList should be null on the first call.
+    //Error detection: User called M_Init more than once.
+    if (freeList != NULL)
     {
         printf("Failure: M_Init was called twice!\n");
         return -1;
@@ -26,65 +32,31 @@ int M_Init(int size)
     }
     memChunks = memChunks * 16;
 
-    printf("using mmap.\n");
-
-
     //Create the mmap space for storing nodes.
     freeList = mmap((void *) 0xdead0000, memChunks, PROT_READ |
                                          PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0);
-
     if(freeList == MAP_FAILED) //Check and report failure to allocate.
     {
         printf("Mapping Failed\n");
         return -1;
     }
+
     //Define global variables for magic number, and the total size of freeList.
     magicNumber = (void *) 123456789;
-    freeListSize = (int)(memChunks - (sizeof(memStruct)));
-
-    printf("Global variables declared.\n");
+    freeListSize = memChunks;
 
     //Define the first header.
-    memStruct *list = freeList;
-    list->memptr = magicNumber;
-    list->size = freeListSize;
+    memStruct *head = freeList;
+    head->memptr = magicNumber;
+    head->size = freeListSize;
+
     //Define the last footer.
-    memStruct *end = freeList + (freeListSize-16);
+    memStruct *end = freeList + (freeListSize);
     end->size = 0;
     end->memptr = freeList;
 
     //Set current at the start of the freeList.
     currentBlock = freeList;
 
-    printf("Returning 0.\n");
     return 0;
 }
-
-
-
-int main(int argc, char *argv[])
-{
-    int givenSize = 4002;
-    printf("Given size: %d\n",givenSize);
-    M_Init(givenSize);
-
-
-    memStruct *header = freeList;
-    memStruct *footer = freeList + freeListSize - 16;
-
-    printf("FreeListSize = %d\n",freeListSize);
-    printf("FreeList Start: %p\n",freeList);
-
-    printf("FreeList header -- size: %lu\n",header->size);
-    printf("FreeList header -- pointer: %p\n",header->memptr);
-
-    printf("FreeList footer -- size: %lu\n",footer->size);
-    printf("FreeList footer -- pointer: %p\n",footer->memptr);
-
-    printf("FreeList current -- Starting address: %p\n",currentBlock);
-    printf("FreeList current -- size: %lu\n",currentBlock->size);
-    printf("FreeList current -- pointer: %p\n",currentBlock->memptr);
-
-    return 0;
-}
-
