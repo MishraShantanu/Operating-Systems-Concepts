9bf4abb162bc6c7c21008c1ba109dcd72183a4bd
Spencer <spt631@usask.ca>
2021-12-04 15:42:41 -0600

    Made handleSender() print and return the time at which the message was received by the server (to facilitate broadcast to receivers later).

--- Assignment_4/server.c
+++ Assignment_4/server.c
@@ -6,35 +6,35 @@
 #define BACKLOG 10
 #define MAXMESSAGELENGTH 1000
 //Start server
-    //Print ports.
-    //Start the listeners.
+//Print ports.
+//Start the listeners.
 
 //Start listeners.
-    //Listen on port 30002 for new senders.
-        //Queue for new senders.
-    //Listen on port 30003 for new receivers.
-        //Queue for new receivers.
+//Listen on port 30002 for new senders.
+//Queue for new senders.
+//Listen on port 30003 for new receivers.
+//Queue for new receivers.
 
 //New sender.
-    //Accept the connection on a new thread.
-    //Wait for a message from sender.
-    //Terminate.
+//Accept the connection on a new thread.
+//Wait for a message from sender.
+//Terminate.
 
 //New receiver.
-    //Accept the connection on a new thread.
-    //wait until there is a message to be received.
-    //Send message to receiver(s).
-    //wait until receiver wants to terminate.
-    //Terminate (determined by receiver).
+//Accept the connection on a new thread.
+//wait until there is a message to be received.
+//Send message to receiver(s).
+//wait until receiver wants to terminate.
+//Terminate (determined by receiver).
 
 //Get message from a sender
-    //Pad message with their IP and port.
-    //Get which receivers are currently connected (and should get this message).
+//Pad message with their IP and port.
+//Get which receivers are currently connected (and should get this message).
 
 //Send message to a receiver.
-    //Get which receivers to send to.
-    //Queue sending the message to all current receivers.
-    //Send to each.
+//Get which receivers to send to.
+//Queue sending the message to all current receivers.
+//Send to each.
 
 void* handleSender(int new_fd, char* givenIP)
 {
@@ -73,24 +73,24 @@ void* handleSender(int new_fd, char* givenIP)
 
 void sigchld_handler(int s)
 {
-	(void)s; // quiet unused variable warning
+    (void)s; // quiet unused variable warning
 
-	// waitpid() might overwrite errno, so we save and restore it:
-	int saved_errno = errno;
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
 
-	while(waitpid(-1, NULL, WNOHANG) > 0);
+    while(waitpid(-1, NULL, WNOHANG) > 0);
 
-	errno = saved_errno;
+    errno = saved_errno;
 }
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 int startServer()
@@ -112,94 +112,94 @@ int startListener(void *portnumber)
     char *PORT = malloc(strlen(portnumber) + 1);;
     strncpy(PORT,portnumber , strlen(portnumber));
 
-    
+
     int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr; // connector's address information
-	socklen_t sin_size;
-	struct sigaction sa;
-	int yes=1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
-    
-    
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+
+
     memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
 
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
     {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next)
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next)
     {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("server: socket");
-			continue;
-		}
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
 
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-				sizeof(int)) == -1) {
-			perror("setsockopt");
-			exit(1);
-		}
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                       sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
 
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("server: bind");
-			continue;
-		}
-		break;
-	}
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+        break;
+    }
 
-	freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(servinfo); // all done with this structure
 
-	if (p == NULL)
+    if (p == NULL)
     {
-		fprintf(stderr, "server: failed to bind\n");
-		exit(1);
-	}
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
 
-	if (listen(sockfd, BACKLOG) == -1)
+    if (listen(sockfd, BACKLOG) == -1)
     {
-		perror("listen");
-		exit(1);
-	}
+        perror("listen");
+        exit(1);
+    }
 
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1)
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1)
     {
-		perror("sigaction");
-		exit(1);
-	}
+        perror("sigaction");
+        exit(1);
+    }
 
-	printf("server: waiting for connections at %s...\n",PORT);
+    printf("server: waiting for connections at %s...\n",PORT);
 
-	while(1)
+    while(1)
     {  // main accept() loop
-		sin_size = sizeof their_addr;
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1)
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1)
         {
-			perror("accept");
-			continue;
-		}
+            perror("accept");
+            continue;
+        }
 
-		inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof s);
-		//printf("server: got connection from %s\n", s);
+        inet_ntop(their_addr.ss_family,
+                  get_in_addr((struct sockaddr *)&their_addr),
+                  s, sizeof s);
+        //printf("server: got connection from %s\n", s);
 
-		if (!fork())
+        if (!fork())
         { // this is the child process
-			close(sockfd); // child doesn't need the listener
+            close(sockfd); // child doesn't need the listener
 
             if (isSender == 1) //Handle receiving messages from sender clients.
             {
@@ -222,11 +222,11 @@ int startListener(void *portnumber)
                     perror("send");
             }
 
-			close(new_fd);
-			exit(0);
-		}
-		close(new_fd);  // parent doesn't need this
-	}
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd);  // parent doesn't need this
+    }
 }
 
 
